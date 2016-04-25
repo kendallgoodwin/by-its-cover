782f27b1e61893e42325beb517a0bcb9db182b68
kendallgoodwin <kendall@kendallgoodwin.com>
2016-04-25 09:34:20 -0700

    initial commit

---
+++ week6/by-its-cover/index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
+// var db = require('./models');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(__dirname + '/static'));
+
+
+app.get('/', function(req, res) {
+	res.render('index');
+})
+
+app.get('/rec', function(req, res) {
+	// var query = req.query.q;
+
+	// request('https://www.googleapis.com/books/v1/volumes?q=' + query, function(err, response, body) {
+	// 	var data = JSON.parse(body);
+	// 	if (!err && response.statusCode === 200 && data.Search) {
+	// 		var results= data.Search;
+	// 		res.render("rec", {results: results})
+	// 	} 
+	// })
+});
+
+app.post('/rec', function(req, res) {
+
+});
+
+app.get('/my-list', function(req, res) {
+
+})
+
+app.post('/my-list', function(req, res) {
+
+})
+
+app.delete('/my-list', function(req, res) {
+	
+})
+
+app.listen(3000);
