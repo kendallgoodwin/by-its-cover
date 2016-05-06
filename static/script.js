abaac2bb3c9cdc3c24418190da24abb7790fe1cb
kendallgoodwin <kendall@kendallgoodwin.com>
2016-05-06 16:22:55 -0700

    topnav collapses on resize

--- static/script.js
+++ static/script.js
@@ -30,6 +30,22 @@ $( window ).resize(function() {
  		$("#sidebar-container").collapse('show');
     }
 
+
+$( window ).resize(function() {
+    if ($(document).width() <= 991){ 
+ 		$(".navbar").collapse('show');
+    }else{
+    	$(".navbar").collapse('hide');
+    }
+});
+
+    if ($(document).width() < 992){ 
+ 		$(".navbar").collapse('show');
+    }
+
+
+
+
 $('.delete-btn').on('click', function(e) {
 	e.preventDefault();
 	var favoriteId = $(this).attr('data-favorite-id');
